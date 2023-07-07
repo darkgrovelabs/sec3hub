9c42c92218ca144704fd8b934e4c25207167081a
jeromevvb <jeromevvb@gmail.com>
2023-07-07 14:24:47 +0300

    First fetch tentative

--- src/app/(dashboard)/companies/page.tsx
+++ src/app/(dashboard)/companies/page.tsx
@@ -1,7 +1,34 @@
 import CompaniesTable from '@/components/companies/CompaniesTable'
 import Widgets from '@/components/companies/Widgets'
 
-export default function Companies() {
+// TODO:
+export const metadata = {
+  title: 'Audit Companies',
+}
+
+async function getCompanies() {
+  const res = await fetch(
+    'https://backbonez.fly.dev/companies?page=1&limit=10',
+    // revalidate every hour
+    { next: { revalidate: 3600 } }
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Companies() {
+  const data = await getCompanies()
+
+  // console.log(data)
+
   return (
     <>
       <Widgets />
--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,4 +1,9 @@
-export default function Companies() {
+// TODO:
+export const metadata = {
+  title: 'Audit Companies',
+}
+
+export default function Products() {
   return (
     <>
       <p>Hello products</p>
